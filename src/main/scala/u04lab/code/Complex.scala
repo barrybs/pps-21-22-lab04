6edcf5866741e07380023a6a2a564cb5311525a4
Barry Bassi <barry.bassi@studio.unibo.it>
2022-03-22 23:42:45 +0100

    Complex class and object - ok

--- src/main/scala/u04lab/code/Complex.scala
+++ src/main/scala/u04lab/code/Complex.scala
@@ -6,8 +6,13 @@ trait Complex:
   def +(c: Complex): Complex // should implement the sum of two complex numbers..
   def *(c: Complex): Complex // should implement the product of two complex numbers
 
+class ComplexImpl (override val re: Double,
+                   override val im: Double) extends Complex:
+  override def +(c: Complex): Complex = Complex(c.re+re, im+c.im)
+  override def *(c: Complex): Complex = Complex(re*c.re - im*c.im, re*c.im + im*c.re)
+
 object Complex:
-  def apply(re: Double, im: Double): Complex = ??? // Fill here
+  def apply(re: Double, im: Double): Complex = new ComplexImpl (re, im)
 
 @main def checkComplex(): Unit =
   val a = Array(Complex(10, 20), Complex(1, 1), Complex(7, 0))
@@ -15,6 +20,10 @@ object Complex:
   println((c, c.re, c.im)) // (ComplexImpl(18.0,21.0),18.0,21.0)
   val c2 = a(0) * a(1)
   println((c2, c2.re, c2.im)) // (ComplexImpl(-10.0,30.0),-10.0,30.0)
+
+  val cplx1 = Complex(1,1)
+  val cplx2 = Complex(1,1)
+  println (cplx1 == cplx2); //true with "case class" or equals redefinition, false with only "class"
 
 /** Hints:
   *   - implement Complex with a ComplexImpl class, similar to PersonImpl in slides
