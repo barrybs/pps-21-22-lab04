fdbeb1e2d67f7ce14ae8ec63fc58b90e3378108a
Barry Bassi <barry.bassi@studio.unibo.it>
2022-03-26 19:49:45 +0100

    a01a ok

--- src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
+++ src/main/scala/u04lab/polyglot/a01a/LogicsImpl.scala
@@ -1,8 +1,29 @@
 package u04lab.polyglot.a01a
+
 import Logics.*
+import u04lab.polyglot.Pair
+import u04lab.code.List._
+import scala.util.Random
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01a/sol2/ */
 class LogicsImpl(private val size: Int, private val boat: Int) extends Logics:
+  private val FAILURES : Int = 0
+  private var hit : u04lab.code.List[Pair[Int,Int]] = Nil()
+  private val r = Random()
+  private val boatRow = r.nextInt(size)
+  private val boatLeftCol: Int = r.nextInt(size - boat + 1)
+  private var failures: Int = 0
 
-  def hit(row: Int, col: Int) =
-    Result.HIT
+  def hit(row: Int, col: Int): Result =
+    if (row == boatRow && col >= boatLeftCol && col < boatLeftCol + boat)
+      hit = Cons(Pair(row,col),hit)
+      if length(hit) == boat then
+        Result.WON
+      else
+        Result.HIT
+    else
+      failures += 1
+      if failures == FAILURES then
+        Result.LOST
+      else
+        Result.MISS
